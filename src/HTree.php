52e8444dd1a89eef81e3ca9ae14d18cfc818d24e
hugh.li <hugh.li@foxmail.com>
2026-02-27 23:24:19 +0800

    fix: getChildren 使用字符串转换比较，兼容 id=0 及混合类型
    将两侧转为字符串后严格比较，同时解决三个问题：
    1. PHP<8.0 下 "string"==0 的误匹配
    2. 数据库场景中字符串与整型的类型不一致
    3. 节点 id 为 0 时子节点查询正常工作

--- src/HTree.php
+++ src/HTree.php
@@ -262,7 +262,7 @@ class HTree
         /* 先收集一次, 防止父 $nid 不存在不能被收集 */
         foreach ($this->items as $id => $item) {
             $parentValue = $this->getNodeProperty($item, $this->parentKey);
-            if (!empty($parentValue) && $nid == $parentValue) {
+            if ((string)$nid === (string)$parentValue) {
                 $parents[] = $id;
                 $nodes[$id] = $item;
             }
